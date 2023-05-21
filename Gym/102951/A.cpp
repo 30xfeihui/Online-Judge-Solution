8e0ae8094d13419df096c426c414928caf0aaa36
Labib Al Faisal <79709412+30xfeihui@users.noreply.github.com>
2023-05-21 14:09:10 +0600

    Update A.cpp

--- Gym/102951/A.cpp
+++ Gym/102951/A.cpp
@@ -15,39 +15,6 @@ using namespace std;
  **/
 
 /**
- * https://codeforces.com/gym/102951/problem/A
+ *
 **/
 
-int distance(int x, int y){
-	x = x*x;
-	y = y*y;
-
-	return x+y;
-}
-
-int32_t main()
-{
-    ios::sync_with_stdio(false) ; cin.tie(0) ;
-
-    int n; cin >> n;
-	vector<int> x(n), y(n);
-
-	for(auto &i: x) cin>>i;
-	for(auto &i: y) cin>>i;
-
-	int ans = numeric_limits<int>::min();
-	for(int i=0;i<n;i++){
-		for(int j=0;j<n;j++){
-			
-			int xx = x[i]-x[j];
-			int yy = y[i]-y[j];
-
-			ans = max(ans,distance(xx, yy));
-
-		}
-	}
-
-	cout << ans << endl;
-	
-    return 0;
-}
