ebb47531b955aa0a710f5ddfdd893f5f68043bd7
Labib Al Faisal <79709412+30xfeihui@users.noreply.github.com>
2023-05-21 17:34:28 +0600

    Add files via upload

---
+++ Yosupo/Matrix/matrix_product.cpp
@@ -0,0 +1,121 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define endl "\n"
+#define LSONE(x) ((x) & -(x))
+#define ONBIT(x,k) ((x) | (1<<k))
+#define OFFBIT(x,k) ((x) & (~(1<<k)))
+#define IFONBIT(x,k) ((x) & (1<<k))
+
+/**
+ * 
+ * 
+ * 9xZER0
+ * 
+ **/
+
+const int mod = 998244353;
+
+struct Mat {
+  int n, m;
+  vector<vector<int>> a;
+  Mat() { }
+  Mat(int _n, int _m) {n = _n; m = _m; a.assign(n, vector<int>(m, 0)); }
+  Mat(vector< vector<int> > v) { n = v.size(); m = n ? v[0].size() : 0; a = v; }
+  inline void make_unit() {
+    assert(n == m);
+    for (int i = 0; i < n; i++)  {
+      for (int j = 0; j < n; j++) a[i][j] = i == j;
+    }
+  }
+  inline Mat operator + (const Mat &b) {
+    assert(n == b.n && m == b.m);
+    Mat ans = Mat(n, m);
+    for(int i = 0; i < n; i++) {
+      for(int j = 0; j < m; j++) {
+        ans.a[i][j] = (a[i][j] + b.a[i][j]) % mod;
+      }
+    }
+    return ans;
+  } 
+  inline Mat operator - (const Mat &b) {
+    assert(n == b.n && m == b.m);
+    Mat ans = Mat(n, m);
+    for(int i = 0; i < n; i++) {
+      for(int j = 0; j < m; j++) {
+        ans.a[i][j] = (a[i][j] - b.a[i][j] + mod) % mod;
+      }
+    }
+    return ans;
+  }
+  inline Mat operator * (const Mat &b) {
+    assert(m == b.n);
+    Mat ans = Mat(n, b.m);
+    for(int i = 0; i < n; i++) {
+      for(int j = 0; j < b.m; j++) {
+        for(int k = 0; k < m; k++) {
+          ans.a[i][j] = (ans.a[i][j] + 1LL * a[i][k] * b.a[k][j] % mod) % mod;
+        }
+      }
+    }
+    return ans;
+  }
+  inline Mat pow(long long k) {
+    assert(n == m);
+    Mat ans(n, n), t = a; ans.make_unit();
+    while (k) {
+      if (k & 1) ans = ans * t;
+      t = t * t;
+      k >>= 1;
+    }
+    return ans;
+  }
+  inline Mat& operator += (const Mat& b) { return *this = (*this) + b; }
+  inline Mat& operator -= (const Mat& b) { return *this = (*this) - b; }
+  inline Mat& operator *= (const Mat& b) { return *this = (*this) * b; }
+  inline bool operator == (const Mat& b) { return a == b.a; }
+  inline bool operator != (const Mat& b) { return a != b.a; }
+};
+
+/**
+
+Mat a(n, m);
+for (int i = 0; i < n; i++) {
+    for (int j = 0; j < m; j++) {
+      cin >> a.a[i][j];
+    }
+}
+
+Mat ans = a * b;
+Mat ans = a.pow(100);
+
+**/
+
+int32_t main()
+{
+    ios::sync_with_stdio(false) ; cin.tie(0) ;
+
+    int n,m,k; cin >> n>>m>>k;
+
+    Mat a(n,m), b(m,k);
+	
+	for (int i = 0; i < n; i++) {
+	    for (int j = 0; j < m; j++) {
+	      cin >> a.a[i][j];
+	    }
+	}
+	for (int i = 0; i < m; i++) {
+    	for (int j = 0; j < k; j++) {
+      		cin >> b.a[i][j];
+    	}
+	}
+
+	Mat ans = a*b;
+
+	for(auto i: ans.a){
+		for(auto j: i) cout << j << " ";
+		cout << endl;
+	}
+
+    return 0;
+}
